36f9b268010417ec86f5ab99ed31cdd878ceba99
Lamardav <d.shpakov@dextechnology.com>
2022-05-05 21:53:23 +0300

    добавил саспенс и пустой экран

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { FC, Suspense } from "react";
 import { MainRoutes } from "./core/routes/mainRoutes";
 import { BrowserRouter } from "react-router-dom";
-import { EmptyScreen } from "./components/emptyScreens/emptyScreen";
+import { LoadingScreen } from "./components/emptyScreens/emptyScreen";
 
 export const App: FC = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<EmptyScreen />}>
+      <Suspense fallback={<LoadingScreen />}>
         <MainRoutes />
       </Suspense>
     </BrowserRouter>
---
+++ src/assets/theme/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  rubberSize: {
+    desktop: "1199px",
+    tablet: "767px",
+    phone: "375px",
+  },
+  colors: {
+    red: "#CC122D",
+    black: "#0D1116",
+    blackLight: "#1B232C",
+    blackOpacity: "rgba(255, 255, 255, 0.2);",
+    grayDark: "#5C6168",
+    gray: "#A5ACB8",
+    grayLight: "#DCE1EA",
+  },
+
+  font: { winline: "FCSM-Medium,sans-serif" },
+};
